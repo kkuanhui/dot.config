448d4871f5a0434dfd5d7f752d2a7a15230bb9c9
KuanHui <kuanhui.dev@gmail.com>
2024-04-03 11:51:22 +0800

    alter setting

--- nvim/init.lua
+++ nvim/init.lua
@@ -115,10 +115,15 @@ local plugins = {
 		end
 	},
 	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-	},
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+		},
 	{
 		'akinsho/bufferline.nvim', 
 		version = "*", 
