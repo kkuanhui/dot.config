50bc2b292de6c3539cedc96beef0ab1603b30e35
KuanHui <kuanhui.dev@gmail.com>
2024-09-20 17:50:42 +0800

    add lsp and autocompletion

---
+++ nvim/lua/plugins/general-setup.lua
@@ -0,0 +1,168 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				color_overrides = {
+					all = {
+						base = "#000000",
+					},
+				},
+				custom_highlights = function(colors)
+					return {
+						LineNr = { fg = "#737373" },
+						Comment = { fg = "#adadad" },
+					}
+				end,
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = {
+						"node_modules",
+					},
+					mappings = {
+						n = {
+							["<c-d>"] = require("telescope.actions").delete_buffer,
+						}, -- n
+						i = {
+							["<C-h>"] = "which_key",
+							["<c-d>"] = require("telescope.actions").delete_buffer,
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"LukasPietzschmann/telescope-tabs",
+		config = function()
+			require("telescope").load_extension("telescope-tabs")
+			require("telescope-tabs").setup({
+				close_tab_shortcut_i = "<C-d>",
+				close_tab_shortcut_n = "D",
+				show_preview = true,
+				entry_ordinal = function(tab_id, buffer_ids, file_names, file_paths, is_current)
+					return table.concat(file_names, " ")
+				end,
+				entry_formatter = function(tab_id, buffer_ids, file_names, file_paths, is_current)
+					local entry_string = table.concat(file_names, ", ")
+					return string.format("%d: %s%s", tab_id, entry_string, is_current and " <" or "")
+				end,
+			})
+		end,
+		dependencies = { "nvim-telescope/telescope.nvim" },
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"preservim/nerdtree",
+		-- loaded = true,
+		-- keys = {{'n', '<leader>n'}},
+		-- config = function()
+		-- 		vim.cmd('NERDTree')
+		-- end
+	},
+	-- {
+	-- 		"nvim-neo-tree/neo-tree.nvim",
+	-- 		branch = "v3.x",
+	-- 		dependencies = {
+	-- 				"nvim-lua/plenary.nvim",
+	-- 				"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+	-- 				"MunifTanjim/nui.nvim",
+	-- 				-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	-- 		},
+	-- 		window = {
+	-- 				position="left",
+	-- 				width= 40,
+	-- 		},
+	-- 		config = function()
+	-- 				require('neo-tree').setup({
+	-- 						filesystem = {
+	-- 								filtered_items = {
+	--           hide_dotfiles = false,
+	--           hide_gitignored = false,
+	-- 								},
+	-- 								never_show = {
+	-- 										-- remains hidden even if visible is toggled to true, this overrides always_show
+	-- 										".DS_Store",
+	-- 										"thumbs.db"
+	-- 								},
+	-- 						}
+	-- 				})
+	-- 		end
+	-- },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
+		"yorickpeterse/nvim-window",
+		keys = {
+			{
+				"<leader>ww",
+				"<cmd>lua require('nvim-window').pick()<cr>",
+				desc = "nvim-window: Jump to window",
+			},
+		},
+		config = true,
+	},
+	{
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			require("nvim-tmux-navigation").setup({
+				disable_when_zoomed = true, -- defaults to false
+				keybindings = {
+					left = "<C-h>",
+					down = "<C-j>",
+					up = "<C-k>",
+					right = "<C-l>",
+					-- last_active = "<C-\\>",
+					-- next = "<C-Space>",
+				},
+			})
+		end,
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			vim.opt.termguicolors = true
+			require("bufferline").setup({})
+		end,
+	},
+	{
+		"mg979/vim-visual-multi",
+		branch = "master",
+	},
+}
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,26 +1,68 @@
--- learn from typecraft lsp series
+-- learned from typecraft lsp series
+-- https://github.com/cpow/neovim-for-newbs
 return {
-		{
-				"williamboman/mason.nvim",
-				config = function()
-						require("mason").setup()
-				end
-		},
-		{
-				"williamboman/mason-lspconfig.nvim",
-				config = function()
-						require("mason-lspconfig").setup({
-								ensure_installed = {"lua_ls", "tsserver"}
-						})
-				end
-		},
-		{
-				"neovim/nvim-lspconfig",
-				config = function()
-						local lspconfig = require("lspconfig")
-						lspconfig.lua_ls.setup({})
-						lspconfig.tsserver.setup({})
-				end
-		}
-}
 
+	{
+		-- used to config lsp in nvim
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+
+	{
+		-- language server manage tool
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+
+	{
+		-- used to config ls in mason to do whatever we want
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+		config = function()
+			local masonLC = require("mason-lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			masonLC.setup({
+				ensure_installed = { "lua_ls", "ts_ls" },
+			})
+			masonLC.setup_handlers({
+				-- loop through all installed language server then bind each into nvim by using "lspconfig"
+				function(language_server_name)
+					lspconfig[language_server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+			})
+		end,
+	},
+
+	{
+		-- wrap non-lsp commond lins tools, such as "eslint", to become generalized lsp
+		"nvimtools/none-ls.nvim", -- null-ls seems deprecated. none-ls is an alternative
+		lazy = false,
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua, -- formatter "stylua" is required. install using Mason
+					require("none-ls.diagnostics.eslint"), -- imported from dependencies
+					require("none-ls.formatting.eslint_d"), -- imported from dependencies
+				},
+			})
+			vim.keymap.set("n", "=", ":lua vim.lsp.buf.format()<CR>", { noremap = true, silent = true })
+		end,
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+	},
+}
