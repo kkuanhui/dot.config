cbf0075a46f7d191fc7eec6bbbdff6cede5bc06d
Kuan Hui, Wu <tf00604520@gmail.com>
2024-11-05 16:09:17 +0800

    add vim setting

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -25,6 +25,7 @@ vim.cmd("set number")
 vim.cmd("set relativenumber")             
 
 -- tab size
+vim.cmd("set expandtab")
 vim.cmd("set tabstop=1")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
