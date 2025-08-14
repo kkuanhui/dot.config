46839c21f20b06fa227fe731863a8365faf4a589
KuanHui Wu <kuanhuiwu@KuanHuis-MacBook-Pro.local>
2025-08-14 21:39:22 +0800

    update

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -2,68 +2,61 @@
 -- https://github.com/cpow/neovim-for-newbs
 return {
 
-	{
-		-- used to config lsp in nvim
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+  {
+    -- used to config lsp in nvim
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      -- vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 
-	{
-		-- language server manage tool
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
+  {
+    -- language server manage tool
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
 
-	{
-		-- used to config ls in mason to do whatever we want
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-		config = function()
-			local masonLC = require("mason-lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			masonLC.setup({
-				ensure_installed = { "lua_ls", "ts_ls" },
-			})
-			masonLC.setup_handlers({
-				-- loop through all installed language server then bind each into nvim by using "lspconfig"
-				function(language_server_name)
-					lspconfig[language_server_name].setup({
-						capabilities = capabilities,
-					})
-				end,
-			})
-		end,
-	},
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  },
 
-	{
-		-- wrap non-lsp commond lins tools, such as "eslint", to become generalized lsp
-		"nvimtools/none-ls.nvim", -- null-ls seems deprecated. none-ls is an alternative
-		lazy = false,
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua, -- formatter "stylua" is required. install using Mason
-					require("none-ls.diagnostics.eslint"), -- imported from dependency none-ls-extras
-					require("none-ls.formatting.eslint"), -- imported from dependency none-ls-extras
-					require("none-ls.code_actions.eslint"), -- imported from dependency none-ls-extras
-				},
-			})
-			vim.keymap.set("n", "=", ":lua vim.lsp.buf.format()<CR>", { noremap = true, silent = true })
-		end,
-		dependencies = {
-			"nvimtools/none-ls-extras.nvim",
-		},
-	},
+  {
+    -- Wrap non-LSP CLI tools, such as "eslint", into LSP sources
+    "nvimtools/none-ls.nvim", -- null-ls is deprecated; none-ls is the alternative
+    lazy = false,
+    config = function()
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,       -- Lua formatter
+          require("none-ls.diagnostics.eslint_d"),  -- ESLint diagnostics
+          require("none-ls.formatting.eslint_d"),   -- ESLint formatting
+          require("none-ls.code_actions.eslint_d"), -- ESLint code actions
+        },
+      })
+
+      -- Manual format keybindings
+      local opts = { noremap = true, silent = true }
+
+      -- Format entire buffer
+      vim.keymap.set("n", "=", function()
+        vim.lsp.buf.format({ async = false })
+        print("Formatting file...")
+      end, vim.tbl_extend("force", opts, { desc = "Format buffer" }))
+    end,
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+  }
 }
