bf249365aa23e68c0b63df485ec4f2ac92ac24b6
Akira Ngoo <kuanhui.dev@gmail.com>
2025-12-15 11:21:00 +0800

    update nvim split window config and make lazygit ask to leave

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -64,6 +64,9 @@ vim.keymap.set("n", "]t", "<cmd>tabnext<cr>", { desc = "Next tab" })
 vim.keymap.set("n", "<leader>w|", "<C-W>s", { desc = "Split window below", remap = true })
 vim.keymap.set("n", "<leader>w_", "<C-W>v", { desc = "Split window right", remap = true })
 
+-- split window at the right side
+vim.opt.splitright = true
+
 -- Resize window using <ctrl> arrow keys
 vim.keymap.set("n", "˚", "<cmd>resize +2<cr>", { desc = "Increase window height" })
 vim.keymap.set("n", "∆", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
